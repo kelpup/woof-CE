c0a567299cfebe719ad46ebf575fcd06bfdecbd1
JF Boueri <jeanfboueri@gmail.com>
2022-03-23 22:58:34 -0400

    #15 - Changing file locations

--- woof-code/rootfs-skeleton/root/Startup/installvim.sh
+++ woof-code/rootfs-skeleton/root/Startup/installvim.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Installing the VIM Stuff
-pkg install /root/vim-common_8.1.2269-1ubuntu5_all.deb
-sleep(5)
-pkg install /root/vim-runtime_8.1.2269-1ubuntu5_all.deb
-sleep(5)
-pkg install /root/vim_8.1.2269-1ubuntu5_amd64.deb
-sleep(5)
-pkg install /root/xxd_8.1.2269-1ubuntu5_amd64.deb
-sleep(5)
-
-touch scriptran.txt
+pkg install vim-common_8.1.2269-1ubuntu5_all.deb
+pkg install vim-runtime_8.1.2269-1ubuntu5_all.deb
+pkg install vim_8.1.2269-1ubuntu5_amd64.deb
+pkg install xxd_8.1.2269-1ubuntu5_amd64.deb
 
 # Delete the directory
 #rm -rf /root/vimtest/
