6c300d0e9450a939e72d9277ed86cdaeecf957f4
JF Boueri <jeanfboueri@gmail.com>
2022-03-23 22:06:42 -0400

    #15 - Trying stuff for vim

--- woof-code/rootfs-skeleton/root/Startup/installvim.sh
+++ woof-code/rootfs-skeleton/root/Startup/installvim.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Installing the VIM Stuff
-pkg install /root/vimtest/vim-common_8.1.2269-1ubuntu5_all.deb
-pkg install /root/vimtest/vim-runtime_8.1.2269-1ubuntu5_all.deb
-pkg install /root/vimtest/vim_8.1.2269-1ubuntu5_amd64.deb
-pkg install /root/vimtest/xxd_8.1.2269-1ubuntu5_amd64.deb
+pkg install /root/vim-common_8.1.2269-1ubuntu5_all.deb
+sleep(5)
+pkg install /root/vim-runtime_8.1.2269-1ubuntu5_all.deb
+sleep(5)
+pkg install /root/vim_8.1.2269-1ubuntu5_amd64.deb
+sleep(5)
+pkg install /root/xxd_8.1.2269-1ubuntu5_amd64.deb
+sleep(5)
+
+touch scriptran.txt
 
 # Delete the directory
-rm -rf /root/vimtest/
+#rm -rf /root/vimtest/
