8474b07ffe69c183d98361975840c5cc181209aa
JF Boueri <jeanfboueri@gmail.com>
2022-03-24 00:25:27 -0400

    #15 - installation scripts for vim

---
+++ woof-code/rootfs-skeleton/root/installvim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Installing VIM
+pkg install /root/Startup/vim-common_8.1.2269-1ubuntu5_all.deb
+pkg install /root/Startup/vim-runtime_8.1.2269-1ubuntu5_all.deb
+pkg install /root/Startup/vim_8.1.2269-1ubuntu5_amd64.deb
+pkg install /root/Startup/xxd_8.1.2269-1ubuntu5_amd64.deb
---
+++ woof-code/rootfs-skeleton/root/removevimpackages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin
+
+# Delete the VIM packages
+
+rm /root/Startup/vim-common_8.1.2269-1ubuntu5_all.deb
+rm /root/Startup/vim-runtime_8.1.2269-1ubuntu5_all.deb
+rm /root/Startup/vim_8.1.2269-1ubuntu5_amd64.deb
+rm /root/Startup/xxd_8.1.2269-1ubuntu5_amd64.deb
